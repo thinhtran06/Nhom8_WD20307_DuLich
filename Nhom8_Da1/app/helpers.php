03c00b4840022b228da4ba82dd81c0e5bd7b200b
thinhtran06 <keongot2970@gmail.com>
2025-11-15 14:33:29 +0700

    Initial commit with project structure

---
+++ Nhom8_Da1/app/helpers.php
@@ -0,0 +1,6 @@
+<?php
+// app/helpers.php
+
+function base_url($path = '') {
+    return 'http://localhost/Web_DuLich/Nhom8_Da1/' . $path;
+}
---
+++ Nhom8_Da1/views/layouts/footer.php
@@ -0,0 +1,19 @@
+<footer class="site-footer">
+    <div class="footer-container">
+        <div class="footer-about">
+            <h3>Web Du Lịch</h3>
+            <p>Khám phá những tour hấp dẫn, đặt tour nhanh chóng và tiện lợi.</p>
+        </div>
+        <div class="footer-links">
+            <ul>
+                <li><a href="index.php">Trang chủ</a></li>
+                <li><a href="index.php?page=admin_tours&action=list">Tour</a></li>
+                <li><a href="index.php?page=admin_bookings&action=list">Booking</a></li>
+                <li><a href="#">Liên hệ</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>© 2025 Web Du Lịch. All rights reserved.</p>
+    </div>
+</footer>
